0c4fa1d48a5e07a6c491f1dd261ca2cb7cd0b8f5
MalikBinn <31941952+MalikBinn@users.noreply.github.com>
2017-11-28 12:47:47 -0800

    Uploaded for test

--- DocumentIndex.cpp
+++ DocumentIndex.cpp
@@ -54,12 +54,12 @@ void	DocumentFile::Close()
 {
 	//************************************************************************************
 	//	LOCAL DATA
-	
+
 	//************************************************************************************
 	//	EXECUTABLE STATEMENTS
 	file_.close();
 	file_.clear();
-	
+
 	return;
 }
 
@@ -72,10 +72,10 @@ int	DocumentFile::GetPageNumber()
 {
 	//************************************************************************************
 	//	LOCAL DATA
-	
+
 	//************************************************************************************
 	//	EXECUTABLE STATEMENTS
-	
+
 	return(pageNumber_);
 }
 
@@ -89,10 +89,66 @@ string	DocumentFile::GetWord()
 	//************************************************************************************
 	//	LOCAL DATA
 	string	word;
-	
+	bool success;
+
 	//************************************************************************************
 	//	EXECUTABLE STATEMENTS
+    std::string str = " .,:;?!()\"'";
+    while(1){
+        success = GetLine(line_stream_,word, str);
+        if(!success){
+            word = "";
+            break;
+        }
+        else if(word.empty())
+            continue;
+        else{
+            mySetIterator= mySet.find(word);
+            if(mySetIterator == mySet.end()){
+                string::iterator it;
+                for(it= word.begin();it!=word.end();it++)
+                {
+                    if(!isalpha(*it))
+                        break;
+                }
+                if(it == word.end())
+                    break;
+            }
 
+        }
+    }
+	/*
+	if(success == true){
+
+        mySetIterator= mySet.find(word);
+        while(mySetIterator != mySet.end()){
+            if(word.empty())
+            {
+                success = GetLine(line_stream_,word, str);
+                if(!success)break;
+                mySetIterator= mySet.find(word);
+            }
+            else{
+
+                string::iterator it;
+                for(it= word.begin();it!=word.end();it++)
+                {
+                    if(!isalpha(*it))
+                        break;
+                }
+                if(it != word.end())
+                {
+                    success = GetLine(line_stream_,word, str);
+                    if(!success)break;
+                    mySetIterator= mySet.find(word);
+                }
+            }
+
+        }
+	}
+	else
+        word = "";
+    */
 	return(word);
 }
 
@@ -106,10 +162,28 @@ bool	DocumentFile::LoadExclusions(const string& name)
 	//************************************************************************************
 	//	LOCAL DATA
 	bool	success;
-	
+
 	//************************************************************************************
 	//	EXECUTABLE STATEMENTS
-	
+	fstream file2_;
+	file2_.open(name.c_str(), ios::in);
+	if (!file2_.fail())
+	{
+		//	You may add any useful initialization here.
+        string word;
+        while(GetLine(file2_, word))
+        {
+            mySetIterator = mySet.find(word);
+            if(mySetIterator == mySet.end())
+                mySet.insert(word);
+        }
+        file2_.close();
+		success = true;
+	}
+	else
+	{
+		success = false;
+	}
 	return(success);
 }
 
@@ -125,7 +199,7 @@ bool	DocumentFile::Open(const string& name)
 
 	//************************************************************************************
 	//	EXECUTABLE STATEMENTS
-	file_.open(name, ios::in);
+	file_.open(name.c_str(), ios::in);
 	if (!file_.fail())
 	{
 		//	You may add any useful initialization here.
@@ -151,6 +225,19 @@ bool	DocumentFile::Read()
 
 	//************************************************************************************
 	//	EXECUTABLE STATEMENTS
+	success = GetLine(file_,text_);
+    if(text_.compare("")==0)
+        {
+        success = GetLine(file_,text_);
+        if(text_.compare("")==0)
+        {
+            success = GetLine(file_,text_);
+            pageNumber_++;
+        }
+    }
+
+    line_stream_.str(text_);
+    line_stream_.clear();
 
 	return(success);
 }
@@ -164,9 +251,57 @@ void	DocumentIndex::Create(DocumentFile& documentFile)
 {
 	//************************************************************************************
 	//	LOCAL DATA
-	
+
 	//************************************************************************************
 	//	EXECUTABLE STATEMENTS
+
+	bool			success;
+
+	string			word;
+
+	//************************************************************************************
+	//	EXECUTABLE STATEMENTS
+
+	while (true)
+	{
+		success = documentFile.Read();
+		if (!success)
+		{
+			break;
+		}
+
+		while (true)
+		{
+			word = documentFile.GetWord();
+			if (!word.empty())
+			{
+			    myMapIterator2 = myMap2.find(word);
+			    if(myMapIterator2 != myMap2.end())
+                {
+                    myMap2[word] = myMap2[word] + 1;
+                    if(myMap2[word]<=10){
+                        myMapIterator = myMap.find(word);
+                        myMap[word].insert(documentFile.GetPageNumber());
+                    }
+                    else{
+                        myMap2.erase(myMapIterator2);
+                        myMapIterator = myMap.find(word);
+                        myMap.erase(myMapIterator);
+                    }
+                }
+                else{
+                    myMap2.insert(pair<string,int>(word, 1));
+                    set<int> mySet;
+                    mySet.insert(documentFile.GetPageNumber());
+                    myMap.insert(pair<string, set<int>>(word, mySet));
+                }
+			}
+			else
+			{
+				break;
+			}
+		}
+	}
 
 	return;
 }
@@ -180,9 +315,23 @@ void	DocumentIndex::Write(ostream& indexStream)
 {
 	//************************************************************************************
 	//	LOCAL DATA
-	
+
 	//************************************************************************************
 	//	EXECUTABLE STATEMENTS
-	
+	for(myMapIterator = myMap.begin(); myMapIterator != myMap.end();myMapIterator++)
+    {
+        indexStream << myMapIterator->first;
+        set<int> mySet = myMapIterator->second;
+        set<int>::iterator it;
+        for(it = mySet.begin();it != mySet.end();it++)
+        {
+            if(it == mySet.begin())
+                indexStream << " "<<*it;
+            else
+                indexStream << ", "<<*it;
+        }
+        indexStream <<endl;
+    }
+
 	return;
 }
--- DocumentIndex.h
+++ DocumentIndex.h
@@ -7,6 +7,7 @@
 //
 //****************************************************************************************
 #include	<fstream>
+#include    <sstream>
 #include	<map>
 #include	<set>
 #include	<string>
@@ -30,32 +31,39 @@ class	DocumentFile
 {
 	public:
 		DocumentFile() : pageNumber_(1) { }
-		
+
 		void	Close();
-		
+
 		int		GetPageNumber();			//	Returns the current pge number.
-		
+
 		string	GetWord();					//	Returns the next legal word not on the exception list;
 											//	returns an empty string if there are no more words in
 											//	the line.
-		
+
 		bool	LoadExclusions(const string& name);	//	Loads a list of words to be excluded from the index
 													//	from a file of the given name.
-		
+
 		bool	Open(const string& name);	//	Opens a document file of the given name.
-		
+
 		bool	Read();						//	Reads the next line of the document file, skipping over
 											//	the double empty lines that mark page separations.
 											//	Returns false if there are no more lines in the file.
-		
+
 	private:
 		StringSize	beginPosition_;
-		
+
 		fstream		file_;
-		
+
 		int			pageNumber_;
-		
+
 		string		text_;
+
+		stringstream   line_stream_;
+
+		typedef set<string> MySet;
+typedef MySet::iterator MySetIterator;
+        MySet mySet;
+        MySetIterator mySetIterator;
 };
 
 class	DocumentIndex
@@ -69,8 +77,18 @@ class	DocumentIndex
 											//	The argument is a stream so that this function
 											//	can be called to wrtite its output to cout for
 											//	test purposes.
-	
+
 	private:
+
+	    typedef map<string,set<int>> MyMap;
+        typedef MyMap::iterator MyMapIterator;
+        MyMap myMap;
+        MyMapIterator myMapIterator;
+
+        typedef map<string,int> MyMap2;
+        typedef MyMap2::iterator MyMapIterator2;
+        MyMap2 myMap2;
+        MyMapIterator2 myMapIterator2;
 
 };
 
